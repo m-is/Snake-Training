8e55a4a37e5abdce9c5aa3a28283d4c9265e4f71
m-istvan <56458289+m-istvan@users.noreply.github.com>
2022-06-10 18:19:07 -0700

    Update GASnake.java
    This class never got used in main, GA algorithm is implemented in Cody-GA branch

--- Snake Training/src/GASnake.java
+++ Snake Training/src/GASnake.java
@@ -1,6 +1,4 @@
 public class GASnake extends snake{
-    //with local sensors there are 243 states 3^5 (one option for empty square, one for food, one for wall)
-    //the snake's own body is considered a wall
-    //not all of the states are possible obviously, but should still be mapped
+    //Did not end up using this class
     int[] policy = new int[243];
 }
